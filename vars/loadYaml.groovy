a7a816815517468e25cce8c7507a6d6f92e4f23c
Sergey Tsurankov <41790609+tsurankov@users.noreply.github.com>
2021-01-31 23:29:59 +0300

    Update loadYaml.groovy

--- vars/loadYaml.groovy
+++ vars/loadYaml.groovy
@@ -1,8 +1,9 @@
 def call(Map config) {
     def yaml = libraryResource('pod-templates/agent.yaml')
     def parseYaml = readYaml text: yaml
-    //print parseYaml
-    //parseYaml.spec.container[0].name = "overrided-name"
-   
-return parseYaml
+    print parseYaml
+    writeYaml file: 'tmp.yaml', data: parseYaml
+    def readResult = new File('tmp.yaml').text
+    print readResult
+return readResult
 }
