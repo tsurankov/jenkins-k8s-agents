6bc31a7390704393b124669a72b6c1467f1d1bdf
Sergey Tsurankov <41790609+tsurankov@users.noreply.github.com>
2021-01-31 23:44:38 +0300

    Update loadYaml.groovy

--- vars/loadYaml.groovy
+++ vars/loadYaml.groovy
@@ -2,8 +2,7 @@ def call(Map config) {
     def yaml = libraryResource('pod-templates/agent.yaml')
     def parseYaml = readYaml text: yaml
     print parseYaml
-    //writeYaml file: 'tmp.yaml', data: parseYaml
-    def readResult = yamlToString(parseYaml)
+    def readResult = writeYaml returnText: true, data: parseYaml
     print readResult
 return readResult
 }
