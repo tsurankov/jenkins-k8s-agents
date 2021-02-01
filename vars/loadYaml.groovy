707b8da0d3cf9b91c7ec06014ccca7d90d3a53fe
Sergey Tsurankov <41790609+tsurankov@users.noreply.github.com>
2021-02-01 10:53:15 +0300

    Update loadYaml.groovy

--- vars/loadYaml.groovy
+++ vars/loadYaml.groovy
@@ -2,6 +2,8 @@ def call(Map config) {
     def yaml = libraryResource('pod-templates/agent.yaml')
     def parseYaml = readYaml text: yaml
     // modify yaml
-    String resultYaml = writeYaml returnText: true, data: parseYaml
+
+    // Has not been released yet https://issues.jenkins.io/browse/JENKINS-63719
+    // String resultYaml = writeYaml returnText: true, data: parseYaml
 return resultYaml
 }
